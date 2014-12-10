fffdb6b2aa0343826a10160fc453d23c38600bf8
AxingguChen <axinggu@qq.com>
2014-12-10 21:39:32 +0100

    read from txt(json form) to string and string to python object

---
+++ YoshiViz/input/input.py
@@ -0,0 +1,13 @@
+import json
+#file name
+file_directory = 'E:\studying software\Python3.4.2\python_file\input.txt'
+#open file
+f=open(file_directory)
+#temp(string) txt to string
+temp = f.read()
+#string to python object
+data = json.loads(temp)
+#print related object
+print (data['DataQuality'][0]['RepoOwner'],data['DataQuality'][0]['monthlyEventsStdDev'])
+print (data['DataCommunity'][0]['RepoOwner'],data['DataCommunity'][0]['avgMilestonesPeriod'])
+
