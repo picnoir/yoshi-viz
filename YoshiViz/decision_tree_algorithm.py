e773ab13d4b7737962785ec90a12b2334c0dffa4
AxingguChen <axinggu@qq.com>
2014-12-12 19:01:12 +0100

    algorithm function add
    test successfully and need to be check the correctness of result

--- YoshiViz/decision_tree_algorithm.py
+++ YoshiViz/decision_tree_algorithm.py
@@ -1,12 +1,54 @@
 import json
 from __init__ import *
 
+
+def DecisionTreeAlgorithm(uniqueComments, avgSubscriptions, avgeUserCommits, avgUserCollaborationFiles, avgeFileContributors, avgMilestonesPeriod, hierarchyDegree, hasWiki, avgDistance, avgCulturalDistance, selfSimilarity):
+    CommunityType = []
+    #Situatedness
+    if Situatedness(avgDistance, avgCulturalDistance):
+        CommunityType.append('CoP')
+        return CommunityType
+    else:
+        #Informality
+        if Informality(avgMilestonesPeriod, hierarchyDegree, hasWiki):
+            CommunityType.append('IN')
+            #Engagement
+            if Engagement(uniqueComments, avgSubscriptions, avgeUserCommits, avgUserCollaborationFiles, avgeFileContributors):
+                CommunityType.append('IC')
+                return CommunityType
+            #judge if is NoP
+            elif NetworkOfPractice(selfSimilarity):
+                CommunityType.append('NoP')
+                return CommunityType
+            #neither IC and NoP
+            else:
+                return CommunityType
+            
+        else:
+            CommunityType.append('FN')
+            if Engagement(uniqueComments, avgSubscriptions, avgeUserCommits, avgUserCollaborationFiles, avgeFileContributors):
+                CommunityType.append('IC')
+                return CommunityType
+            elif NetworkOfPractice(selfSimilarity):
+                CommunityType.append('NoP')
+                return CommunityType
+            else:
+                return CommunityType
+
+
+
+
+#code below will done in report_generator
 fileDirectory = r'.\input.txt'
 #read from txt
 temp = TxtToString(fileDirectory)
 #string to python object
 data = StringToObj(temp)
-
+#test decision tree
+print('RepositoryOwner RepositoryName type')
+for i in range(len(data['DataCommunity'])):
+    tempCommunityType = DecisionTreeAlgorithm(data['DataCommunity'][i]['uniqueCommenterExists'],data['DataQuality'][i]['avgSubscriptions'],data['DataQuality'][i]['avgeUserCommits'],data['DataQuality'][i]['avgUserCollaborationFiles'],data['DataQuality'][i]['avgeFileContributors'], data['DataCommunity'][i]['avgMilestonesPeriod'],data['DataCommunity'][i]['hierarchyDegree'],data['DataQuality'][i]['hasWiki'], data['DataCommunity'][i]['avgDistance'], data['DataCommunity'][i]['avgCulturalDistance'], data['DataCommunity'][i]['selfSimilarity'])
+    print(data['DataCommunity'][i]['RepoOwner'],data['DataCommunity'][i]['RepoName'],tempCommunityType)
 
 #test
 #print related object
