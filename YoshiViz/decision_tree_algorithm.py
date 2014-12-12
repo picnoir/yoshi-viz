6283705a0ac44fcb369bc57a235d00c5533bd5be
AxingguChen <axinggu@qq.com>
2014-12-12 11:49:23 +0100

    #test high engagement(true is high engagement) #test informality(true is informal) #test situated distance  three test added to test functions modular(successfully)

--- YoshiViz/decision_tree_algorithm.py
+++ YoshiViz/decision_tree_algorithm.py
@@ -3,13 +3,34 @@ from __init__ import *
 
 fileDirectory = r'.\input.txt'
 #read from txt
-temp = txtToString(fileDirectory)
+temp = TxtToString(fileDirectory)
 #string to python object
 data = StringToObj(temp)
 
+
 #test
 #print related object
-print (data['DataQuality'][0]['RepoOwner'],data['DataQuality'][0]['monthlyEventsStdDev'])
-print (data['DataCommunity'][0]['RepoOwner'],data['DataCommunity'][0]['avgMilestonesPeriod'])
+#print (data['DataQuality'][0]['RepoOwner'],data['DataQuality'][0]['monthlyEventsStdDev'])
+#print (data['DataCommunity'][0]['RepoOwner'],data['DataCommunity'][0]['avgMilestonesPeriod'])
 
 
+#test high engagement(true is high engagement)
+'''
+for i in range(len(data['DataCommunity'])):
+    highEngagement = Engagement(data['DataCommunity'][i]['uniqueCommenterExists'],data['DataQuality'][i]['avgSubscriptions'],data['DataQuality'][i]['avgeUserCommits'],data['DataQuality'][i]['avgUserCollaborationFiles'],data['DataQuality'][i]['avgeFileContributors'])
+    print (highEngagement)
+'''
+
+#test informality(true is informal)
+'''
+for i in range(len(data['DataCommunity'])):
+    informal = Informality(data['DataCommunity'][i]['avgMilestonesPeriod'],data['DataCommunity'][i]['hierarchyDegree'],data['DataQuality'][i]['hasWiki'])
+    print (informal)
+'''
+
+#test situated distance
+'''
+for i in range(len(data['DataCommunity'])):
+    situated = Situatedness(data['DataCommunity'][i]['avgDistance'], data['DataCommunity'][i]['avgCulturalDistance'])
+    print (situated)
+'''
