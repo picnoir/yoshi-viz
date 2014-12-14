4a1df80932b403f3e95a5d8540be123a753310f3
AxingguChen <axinggu@qq.com>
2014-12-14 12:05:40 +0100

    add test interface, inputing a name of repository will return the type of this repository. Missing the module of checking the name if exists

--- __main__.py
+++ __main__.py
@@ -1,8 +1,15 @@
-from YoshiViz import  report_generator
+from YoshiViz import    report_generator
+from YoshiViz import    decision_tree_algorithm
 import os
 
+
+#file director        
+fileDirectory = '.\YoshiViz\input.txt'
+#test decision tree
+repositoryName = input("Please input repository name:\n")
+tempCommunityType = decision_tree_algorithm.\
+      CommunityType(fileDirectory,repositoryName)
 if __name__ == '__main__':
     report_generator.\
-        generate_pdf_report(os.path.join(os.path.abspath('.'),
-                                         'YoshiViz', 'input.txt'), 'SignalR', 'Community Type')
-
+        generate_pdf_report(fileDirectory, repositoryName, tempCommunityType)
+    print('the type of', repositoryName,'is', tempCommunityType , '\n"check .\YoshiViz\output"')
